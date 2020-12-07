c40eaf1baa148637a453dcb4419dc229f5a72bbd
maha <maha@gmail.com>
2020-12-07 07:59:28 +0530

    HTML Reports

--- November2020_7AMSeleniumBatch/src/com/launchings/BaseTest.java
+++ November2020_7AMSeleniumBatch/src/com/launchings/BaseTest.java
@@ -15,6 +15,9 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.ProfilesIni;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 public class BaseTest 
 {
 	public static WebDriver driver;
@@ -24,6 +27,8 @@ public class BaseTest
 	public static Properties parentProp;
 	public static Properties childProp;
 	public static Properties orProp;
+	public static ExtentReports report;
+	public static ExtentTest test;
 	
 	
 	public static void init() throws Exception
@@ -50,6 +55,8 @@ public class BaseTest
 		
 		fis = new FileInputStream(projectPath+"/log4jconfig.properties");
 		PropertyConfigurator.configure(fis);
+		
+		report = ExtentManager.getInstance();
 	}
 	
 	public static void launch(String browser)
