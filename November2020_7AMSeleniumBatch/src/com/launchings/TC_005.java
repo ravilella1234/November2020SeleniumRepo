42f7cc06e6c20912d3b1ca7d404f59a12021e8d1
maha <maha@gmail.com>
2020-12-11 07:55:18 +0530

    2nd line added

--- November2020_7AMSeleniumBatch/src/com/launchings/TC_005.java
+++ November2020_7AMSeleniumBatch/src/com/launchings/TC_005.java
@@ -5,6 +5,7 @@ public class TC_005 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first Commit");
+		System.out.println("second Commit");
 	}
 
 }
