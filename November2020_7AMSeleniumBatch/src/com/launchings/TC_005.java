5e52f39676625e58ce8faea479b96994240996cc
maha <maha@gmail.com>
2020-12-16 07:17:41 +0530

    Revert "2nd line added"
    This reverts commit 42f7cc06e6c20912d3b1ca7d404f59a12021e8d1.

--- November2020_7AMSeleniumBatch/src/com/launchings/TC_005.java
+++ November2020_7AMSeleniumBatch/src/com/launchings/TC_005.java
@@ -5,7 +5,6 @@ public class TC_005 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first Commit");
-		System.out.println("second Commit");
 	}
 
 }
